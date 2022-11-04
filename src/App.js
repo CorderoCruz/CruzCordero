f0ce5d006e551c463d11172c64da26a2d17d98fc
Cruz Cordero Leon <105164264+CorderoCruz@users.noreply.github.com>
2022-11-03 20:41:43 -0700

    made portfolio one page applcation:

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import LandingPage from "./pages/LandingPage";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <Router>
-        <Routes>
-          <Route path='/' element={<LandingPage />} />
-        </Routes>
-      </Router>
+      <LandingPage />
     </div>
   );
 }
