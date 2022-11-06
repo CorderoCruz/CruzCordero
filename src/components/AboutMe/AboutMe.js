4a78736dd2ee99365f5de5a7cea6b4054b492d60
Cruz Cordero Leon <105164264+CorderoCruz@users.noreply.github.com>
2022-11-06 10:30:53 -0800

    edited navbar to not take up to much space on screen

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -1,10 +1,22 @@
 import "./AboutMe.css";
+import ProfilePicture from "../../images/Profile-Picture-transparent.jpg";
 
 export default function AboutMe() {
   return (
     <div>
       <div className='aboutme-container'>
-        <div></div>
+        <div className='aboutme-left'>
+          <h2>About Me</h2>
+          <p>
+            Hello there! My name is Cruz Cordero and welcome to my portfolio. I have earned my certificate in the University of California, Riverside
+            Coding Bootcamp in full stack web development.
+          </p>
+        </div>
+        <div className='aboutme-right'>
+          <div className='image-container'>
+            <img src={ProfilePicture}></img>
+          </div>
+        </div>
       </div>
     </div>
   );
